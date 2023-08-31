44911e21bc9e82794fbd80095e0e5fcd21ad9c73
rlunar <rlunar@mercadeoracional.com>
2023-08-31 12:48:43 -0400

    add route test

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -5,6 +5,25 @@ import { scrapearRemax, migrarRemax } from '../controllers/remax.controller';
 import { scrapearPerezRealState } from '../controllers/perez-real-state.controller';
 const router:Router = Router();
 
+class HealthChecker {
+    static check() {
+     return {
+        uptime: process.uptime(),
+        response: process.hrtime(),
+        message: 'OK',
+        timestamp: Date.now()
+      };
+    }
+}
+
+router.get('/', async (_req, res, _next) => {
+    try {
+      res.send(HealthChecker.check());
+    } catch (error) {
+      res.status(503).send(error);
+    }
+});
+
 router.post('/scraping/get/links', getUrlWeb);
 router.post('/scraping/site',scrapeAsincrono );
 router.post('/scraping/web', scrapeSite );
