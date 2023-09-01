4427060ea7f9c70740c0ecf9e36de04ee55ea346
rlunar <rlunar@mercadeoracional.com>
2023-09-01 09:39:06 -0400

    ajuste en appspec.yml 11

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navega al directorio donde se encuentra tu aplicación
+cd /var/www/app/scraping
+
+# Instala las dependencias (si es necesario)
+# Esto asume que tu proyecto utiliza npm como gestor de paquetes
+npm install
+
+# Inicia tu aplicación
+# Reemplaza "node index.js" con el comando real para iniciar tu aplicación
+node index.js
