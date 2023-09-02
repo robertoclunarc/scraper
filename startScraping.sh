3812ec907a800a38c92a3abf44b7f6dae66705e8
rlunar <rlunar@mercadeoracional.com>
2023-09-01 20:46:26 -0400

    ajuste en appspec.yml 22

--- startScraping.sh
+++ startScraping.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Agrega esta línea para cargar nvm y configurar el entorno de Node.js
+source ~/.nvm/nvm.sh
+
 # Navega al directorio donde se encuentra tu aplicación
 cd /var/www/scraper
 
 # Instala las dependencias (si es necesario)
 # Esto asume que tu proyecto utiliza npm como gestor de paquetes
-~/.nvm/versions/node/v16.20.2/bin/npm install
+npm install
 
 # Inicia tu aplicación
 # Reemplaza "node index.js" con el comando real para iniciar tu aplicación
