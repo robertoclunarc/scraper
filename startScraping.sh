cb3bdba02f4f274809ba3a0b96b602d2e3a4e485
rlunar <rlunar@mercadeoracional.com>
2023-09-01 22:00:11 -0400

    ajuste en appspec.yml 27

--- startScraping.sh
+++ startScraping.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Obtiene la ubicación de la implementación actual
-DEPLOYMENT_GROUP_NAME=$(curl https://54.167.26.168/latest/user-data/ -s | tr -d '\n' | sed 's/.*DEPLOYMENT_GROUP_NAME=//')
-
-# Define la ruta de origen de la implementación actual
-DEPLOYMENT_SOURCE="/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_NAME/"
-
-# Copia los archivos de implementación a /var/www/scraper
-cp -r "$DEPLOYMENT_SOURCE" /var/www/scraper/
-
 # Agrega esta línea para cargar nvm y configurar el entorno de Node.js
 source ~/.nvm/nvm.sh
 
